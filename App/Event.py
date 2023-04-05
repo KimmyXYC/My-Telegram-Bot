882e9f926990c9b6a4a7a39467895afb9727a193
エルフです <69386744+KimmyXYC@users.noreply.github.com>
2023-04-05 15:56:38 +0800

    Update Event.py

--- App/Event.py
+++ App/Event.py
@@ -16,16 +16,12 @@ async def InLine_Message(bot, query):
         id='1',
         photo_url=f'https://api.mahiron.moe/hangzhou.jpg?time={timestamp}',
         thumb_url=f'https://api.mahiron.moe/hangzhou.jpg?time={timestamp}',
-        photo_width=1024,
-        photo_height=900,
         caption='Location: 杭州电信'
     )
     photo_result2 = telebot.types.InlineQueryResultPhoto(
         id='2',
         photo_url=f'https://bili.elaina.pub/biliserver/guangzhou.jpg?time={timestamp}',
         thumb_url=f'https://bili.elaina.pub/biliserver/guangzhou.jpg?time={timestamp}',
-        photo_width=1024,
-        photo_height=900,
         caption='Location: 广州百度云'
     )
     await bot.answer_inline_query(query.id, [photo_result1, photo_result2])
