c3624d9f5e4e0fd218034aa9adfa97df51216e8d
エルフです <kimmyxyc@163.com>
2023-07-10 20:10:17 +0800

    fix: Refine Code

--- App/Controller.py
+++ App/Controller.py
@@ -8,22 +8,25 @@ from telebot.asyncio_storage import StateMemoryStorage
 
 
 class BotRunner(object):
-    def __init__(self, config):
+    def __init__(self, config, db):
         self.bot = config.bot
         self.proxy = config.proxy
+        self.db = db
+        self.bot_user = None
 
     def botcreate(self):
         bot = AsyncTeleBot(self.bot.botToken, state_storage=StateMemoryStorage())
         return bot, self.bot
 
     def run(self):
-        # print(self.bot)
         logger.success("Bot Start")
         bot, _config = self.botcreate()
         if self.proxy.status:
             from telebot import asyncio_helper
             asyncio_helper.proxy = self.proxy.url
             logger.success("Proxy Set")
+        self.bot_user = await bot.get_me()
+        logger.success("Bot User Get")
 
         @bot.message_handler(commands=['calldoctor', 'callmtf'])
         async def call_anyone(message):
@@ -32,8 +35,7 @@ class BotRunner(object):
         @bot.message_handler(commands=['lock_cmd'])
         async def lock_command(message):
             if message.chat.type in ['group', 'supergroup']:
-                bot_user = await bot.get_me()
-                bot_member = await bot.get_chat_member(message.chat.id, bot_user.id)
+                bot_member = await bot.get_chat_member(message.chat.id, self.bot_user.id)
                 if bot_member.status == 'administrator' and bot_member.can_delete_messages:
                     chat_member = await bot.get_chat_member(message.chat.id, message.from_user.id)
                     if (chat_member.status == 'administrator' and chat_member.can_delete_messages) \
@@ -43,7 +45,7 @@ class BotRunner(object):
                             await bot.reply_to(message, "格式错误, 格式应为 /lock_cmd [CMD]")
                         elif len(command_args) == 2:
                             cmd = command_args[1]
-                            await Event.lock_command(bot, message, cmd)
+                            await Event.lock_command(bot, message, cmd, self.db)
                         else:
                             await bot.reply_to(message, "格式错误, 格式应为 /lock_cmd [CMD]")
                     else:
@@ -64,7 +66,7 @@ class BotRunner(object):
                         await bot.reply_to(message, "格式错误, 格式应为 /unlock_cmd [CMD]")
                     elif len(command_args) == 2:
                         cmd = command_args[1]
-                        await Event.unlock_command(bot, message, cmd)
+                        await Event.unlock_command(bot, message, cmd, self.db)
                     else:
                         await bot.reply_to(message, "格式错误, 格式应为 /unlock_cmd [CMD]")
                 else:
@@ -78,7 +80,7 @@ class BotRunner(object):
                 chat_member = await bot.get_chat_member(message.chat.id, message.from_user.id)
                 if (chat_member.status == 'administrator' and chat_member.can_delete_messages) \
                         or chat_member.status == 'creator':
-                    await Event.list_locked_command(bot, message)
+                    await Event.list_locked_command(bot, message, self.db)
                 else:
                     await bot.reply_to(message, "您无权使用此功能")
             else:
@@ -87,12 +89,14 @@ class BotRunner(object):
         @bot.message_handler(func=lambda message: message.chat.type in ['group', 'supergroup'], content_types=['text'])
         async def handle_commands(message):
             if message.text.startswith('/'):
-                bot_user = await bot.get_me()
-                bot_member = await bot.get_chat_member(message.chat.id, bot_user.id)
-                if bot_member.status == 'administrator' and bot_member.can_delete_messages:
+                if self.db.exists(str(message.chat.id)):
                     command = message.text.split()[0].lower()
                     command = command[1:]
-                    await Event.handle_command(bot, message, command)
+                    lock_cmd_list = self.db.get(str(message.chat.id))
+                    if lock_cmd_list is None:
+                        lock_cmd_list = []
+                    if command in lock_cmd_list:
+                        await bot.delete_message(message.chat.id, message.message_id)
 
         @bot.inline_handler(lambda query: True)
         async def send_photo(query):
--- App/Event.py
+++ App/Event.py
@@ -2,7 +2,6 @@ import telebot
 import time
 import random
 from loguru import logger
-from Utils.Parameter import get_parameter, save_config
 
 
 async def call_anyone(bot, message):
@@ -26,41 +25,33 @@ async def call_anyone(bot, message):
     await bot.reply_to(message, anyone_msg)
 
 
-async def lock_command(bot, message, cmd):
-    lock_cmd_list = get_parameter(message.chat.id)
+async def lock_command(bot, message, cmd, db):
+    lock_cmd_list = db.get(str(message.chat.id))
     if cmd in lock_cmd_list:
         await bot.reply_to(message, "该命令已被锁定")
     else:
         lock_cmd_list.append(cmd)
-        save_config(message.chat.id, lock_cmd_list)
+        db.set(str(message.chat.id), lock_cmd_list)
         logger.info(f"Lock Command: {cmd} in {message.chat.id}")
         await bot.reply_to(message, f"已锁定命令`{cmd}`", parse_mode='Markdown')
 
 
-async def unlock_command(bot, message, cmd):
-    lock_cmd_list = get_parameter(message.chat.id)
+async def unlock_command(bot, message, cmd, db):
+    lock_cmd_list = db.get(str(message.chat.id))
     if cmd in lock_cmd_list:
         lock_cmd_list.remove(cmd)
-        save_config(str(message.chat.id), lock_cmd_list)
+        db.set(str(message.chat.id), lock_cmd_list)
         logger.info(f"Unlock Command: {cmd} in {message.chat.id}")
         await bot.reply_to(message, f"已解锁命令`{cmd}`", parse_mode='Markdown')
     else:
         await bot.reply_to(message, "该命令未被锁定")
 
 
-async def list_locked_command(bot, message):
-    lock_cmd_list = get_parameter(message.chat.id)
+async def list_locked_command(bot, message, db):
+    lock_cmd_list = db.get(str(message.chat.id))
     msg = "以下命令在本群中被锁定:\n"
     msg += "\n".join(f"- `{item}`" for item in lock_cmd_list)
     await bot.reply_to(message, msg, parse_mode='Markdown')
-
-
-async def handle_command(bot, message, cmd):
-    lock_cmd_list = get_parameter(message.chat.id)
-    if lock_cmd_list is None:
-        lock_cmd_list = []
-    if cmd in lock_cmd_list:
-        await bot.delete_message(message.chat.id, message.message_id)
 
 
 async def inline_message(bot, query):
