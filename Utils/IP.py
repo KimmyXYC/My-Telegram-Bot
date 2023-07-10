93fe72f3f576ddbefb28fa358c2ce53c03cb62b0
エルフです <kimmyxyc@163.com>
2023-07-10 23:24:39 +0800

    fix: IP Check

--- Utils/IP.py
+++ Utils/IP.py
@@ -5,6 +5,7 @@
 # @GitHub: KimmyXYC
 import ipaddress
 import socket
+import requests
 from loguru import logger
 
 
@@ -29,3 +30,39 @@ def get_ip_address(domain):
         logger.error(e)
         return None
 
+
+def ali_ipcity_ip(ip_addr, appcode):
+    url = "https://ipcity.market.alicloudapi.com/ip/city/query"
+    headers = {"Authorization": "APPCODE {}".format(appcode)}
+    params = {"ip": ip_addr}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data["code"] == 200:
+            return True, data["data"]["result"]
+        else:
+            return False, data["msg"]
+
+
+def kimmy_ip(ip_addr):
+    url = "https://api.kimmyxyc.top/ip"
+    params = {"ip": ip_addr}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data["code"] == 0:
+            return True, data["data"]
+        else:
+            return False, data["data"]["error"]
+
+
+def ipapi_ip(ip_addr):
+    url = f"http://ip-api.com/json/{ip_addr}"
+    params = {"lang": "zh-CN"}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data["status"] == "success":
+            return True, data
+        else:
+            return False, data["message"]
