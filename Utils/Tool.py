228d1e171aba7de80e6df884c3b0fac952cd85f3
KimmyXYC <kimmyxyc@gmail.com>
2024-07-22 18:16:54 +0800

    refine(rank): Better emoji filter

--- Utils/Tool.py
+++ Utils/Tool.py
@@ -1,4 +1,5 @@
 import re
+import emoji
 
 
 def escape_md_v2_text(text):
@@ -7,12 +8,4 @@ def escape_md_v2_text(text):
 
 
 def remove_emoji(string):
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # 表情符号
-                               u"\U0001F300-\U0001F5FF"  # 符号和标志
-                               u"\U0001F680-\U0001F6FF"  # 交通工具
-                               u"\U0001F1E0-\U0001F1FF"  # 国旗
-                               u"\U00002702-\U000027B0"  # 杂项符号
-                               "]+", flags=re.UNICODE)
-    text_without_emojis = emoji_pattern.sub(r'', string)
-    return text_without_emojis
+    return emoji.replace_emoji(string, replace="")
