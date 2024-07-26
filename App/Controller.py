8cb8a93e50130f627121d83a784f684d41831c1e
KimmyXYC <kimmyxyc@gmail.com>
2024-07-26 11:29:32 +0800

    feat(info): Server Info

--- App/Controller.py
+++ App/Controller.py
@@ -44,17 +44,17 @@ class BotRunner(object):
             load_avg = psutil.getloadavg()
 
             info_message = (
-                f"*Operating System:* {os_info}\n"
-                f"*CPU Usage:* {cpu_usage}%\n"
-                f"*Memory Usage:* {memory_info.percent}% (Total: {memory_info.total / (1024 ** 3):.2f} GB, "
-                f"Used: {memory_info.used / (1024 ** 3):.2f} GB)\n"
-                f"*Swap Usage:* {swap_info.percent}% (Total: {swap_info.total / (1024 ** 3):.2f} GB, "
-                f"Used: {swap_info.used / (1024 ** 3):.2f} GB)\n"
-                f"*Disk Usage:* {disk_info.percent}% (Total: {disk_info.total / (1024 ** 3):.2f} GB, "
-                f"Used: {disk_info.used / (1024 ** 3):.2f} GB)\n"
-                f"*Network I/O:* Sent: {net_io.bytes_sent / (1024 ** 2):.2f} MB, "
-                f"Received: {net_io.bytes_recv / (1024 ** 2):.2f} MB\n"
-                f"*Load Average:* 1 min: {load_avg[0]:.2f}, 5 min: {load_avg[1]:.2f}, 15 min: {load_avg[2]:.2f}"
+                f"*Operating System:* `{os_info}`\n"
+                f"*CPU Usage:* `{cpu_usage}%`\n"
+                f"*Memory Usage:* `{memory_info.percent}% (Total: {memory_info.total / (1024 ** 3):.2f} GB, "
+                f"Used: {memory_info.used / (1024 ** 3):.2f} GB)`\n"
+                f"*Swap Usage:* `{swap_info.percent}% (Total: {swap_info.total / (1024 ** 3):.2f} GB, "
+                f"Used: {swap_info.used / (1024 ** 3):.2f} GB)`\n"
+                f"*Disk Usage:* `{disk_info.percent}% (Total: {disk_info.total / (1024 ** 3):.2f} GB, "
+                f"Used: {disk_info.used / (1024 ** 3):.2f} GB)`\n"
+                f"*Network I/O:* `Sent: {net_io.bytes_sent / (1024 ** 2):.2f} MB, "
+                f"Received: {net_io.bytes_recv / (1024 ** 2):.2f} MB`\n"
+                f"*Load Average:* `1 min: {load_avg[0]:.2f}, 5 min: {load_avg[1]:.2f}, 15 min: {load_avg[2]:.2f}`"
             )
 
             await bot.reply_to(message, info_message, parse_mode='Markdown')
